75573b077a629ae3dae36e2138e693a580f29ff3
Vadim <v.dudko93@gmail.com>
2021-02-03 18:40:36 +0300

    fix: refactor main.js

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -27,24 +27,3 @@ document.addEventListener('click', (event) => {
 }
 
 hideMenu();
-
-// slider
-
-const buttonsArrow = document.querySelectorAll('.buttons__arrow');
-const ourFriendsBlock = document.getElementById('our-friends__block');
-const slider = ourFriendsBlock.querySelectorAll('.slider');
-let currentSlide = 0;
-
-buttonsArrow.forEach(element => {
-    element.addEventListener('click', () => {
-        ourFriendsBlock.querySelectorAll('.slider').forEach(element => element.classList.remove('slider--active'));
-
-        if(currentSlide < slider.length - 1) {
-            currentSlide ++;
-            slider[currentSlide].classList.add('slider--active');
-        } else {
-            currentSlide = 0;
-            slider[currentSlide].classList.add('slider--active');
-        }
-    });
-});
